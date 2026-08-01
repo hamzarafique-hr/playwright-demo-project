25bcc168bcc14038db3bf3dd8548a8f6dd9c36c0
Hamza Rafique <hamzarafique600@gmail.com>
2026-08-01 14:15:30 +0500

    updated scripts in locators file

---
+++ tests/codegen.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://example.com/');
+  await page.getByRole('heading', { name: 'Example Domain' }).click();
+  await page.getByText('This domain is for use in').click();
+  await page.getByRole('link', { name: 'Learn more' }).click();
+  await page.getByRole('heading', { name: 'Example Domains' }).click();
+  await page.getByRole('link', { name: 'Domains', exact: true }).click();
+  await page.goto('https://example.com/');
+});
