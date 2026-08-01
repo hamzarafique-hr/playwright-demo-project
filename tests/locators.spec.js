f658654a4a1f166d340a4235e93ee2982abdf8fb
Hamza Rafique <hamzarafique600@gmail.com>
2026-08-01 12:29:06 +0500

    updated scripts in locators file

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -1,9 +1,31 @@
 import { test } from '@playwright/test';
 
-test('Go to the Practice Test Automation Website', async ({ page }) => {
+test.skip('Go to the Practice Test Automation Website', async ({ page }) => {
 
-    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.goto('https://practicetestautomation.com');
 
     await page.pause();
 
 });
+
+test.skip('Go to the Practice Test Automation Website and Login', async ({ page }) => {
+
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.getByLabel('Username').fill('student');
+    await page.getByLabel('Password').fill('Password123');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await page.pause();
+    
+});
+
+test('Go to Automation Exercise Website and Login with Locator', async ({ page }) => {
+    await page.goto('https://automationexercise.com/login');
+    await page.locator("input[data-qa='login-email']").fill('hamzawork9d@gmail.com');
+    // await page.getByPlaceholder('Email Address').fill('hamzawork9d@gmail.com');
+    await page.getByPlaceholder('Password').fill('password123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.pause();
+
+
+});
+
